50b7c691a75aec369e035f5c83db717beeac2144
root <root@machine.station>
2024-12-29 08:28:19 -0500

    ver 1.0

---
+++ domusmundi/src/main/java/com/domusmundi/DomusmundiApplication.java
@@ -0,0 +1,13 @@
+package com.domusmundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomusmundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomusmundiApplication.class, args);
+	}
+
+}
---
+++ domusmundi/src/test/java/com/domusmundi/DomusmundiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domusmundi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomusmundiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
